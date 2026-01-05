e7dbec54bfc41f142b83f2292efffdfa07213318
Mikhail <cocacolaespanolaa@gmail.com>
2026-01-05 15:23:08 +0300

    Downgrade AGP to 7.4.2 for legacy plugin namespace compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,26 +16,6 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
-// Workaround for legacy plugins that don't specify namespace in build.gradle
-// Required for AGP 8+ compatibility
-subprojects {
-    afterEvaluate {
-        val androidExtension = extensions.findByName("android")
-        if (androidExtension is com.android.build.gradle.LibraryExtension) {
-            if (androidExtension.namespace.isNullOrEmpty()) {
-                val manifestFile = file("${projectDir}/src/main/AndroidManifest.xml")
-                if (manifestFile.exists()) {
-                    val manifest = groovy.xml.XmlSlurper().parse(manifestFile)
-                    val packageName = manifest.getProperty("@package")?.toString()
-                    if (!packageName.isNullOrEmpty()) {
-                        androidExtension.namespace = packageName
-                    }
-                }
-            }
-        }
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
