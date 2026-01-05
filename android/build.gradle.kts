018d3cddeb7efdcfdaec739dc13f745b711d234b
Mikhail <cocacolaespanolaa@gmail.com>
2026-01-05 15:11:05 +0300

    Fix namespace error for legacy plugins (AGP 8+ compatibility)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,28 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Workaround for legacy plugins that don't specify namespace in build.gradle
+// Required for AGP 8+ compatibility
+subprojects {
+    afterEvaluate { project ->
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android")
+            if (android is com.android.build.gradle.LibraryExtension) {
+                if (android.namespace == null || android.namespace!!.isEmpty()) {
+                    val manifestFile = file("${project.projectDir}/src/main/AndroidManifest.xml")
+                    if (manifestFile.exists()) {
+                        val manifest = groovy.xml.XmlSlurper().parse(manifestFile)
+                        val packageName = manifest.getProperty("@package")?.toString()
+                        if (!packageName.isNullOrEmpty()) {
+                            android.namespace = packageName
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
