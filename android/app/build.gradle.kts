3bb4bc898eb039c2437a6fc04a7bd7fc3419cd1a
Mikhail <cocacolaespanolaa@gmail.com>
2026-01-05 15:56:46 +0300

    Upgrade minSdkVersion to 24 for webview_flutter_android compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,7 @@ android {
         applicationId = "com.example.votepfurapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        minSdkVersion(23)
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
