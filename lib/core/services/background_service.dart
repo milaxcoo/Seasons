c4ed212147e89037f2b925b3ac9f76638db0b040
Mikhail <cocacolaespanolaa@gmail.com>
2026-01-23 17:44:38 +0300

    feat: Add custom notification icon (Vector Drawable)

--- lib/core/services/background_service.dart
+++ lib/core/services/background_service.dart
@@ -143,7 +143,7 @@ void onStart(ServiceInstance service) async {
   final notificationsPlugin = FlutterLocalNotificationsPlugin();
   await notificationsPlugin.initialize(
     const InitializationSettings(
-      android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+      android: AndroidInitializationSettings('@drawable/ic_notification'),
       iOS: DarwinInitializationSettings(),
     ),
   );
